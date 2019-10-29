17c0b06398a2b25eb0c3e8cac10ce685cb58dd6d
Drusketh <zenithdruskin@gmail.com>
2019-10-29 16:21:34 -0700

    Made Screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,54 @@
 #include "main.h"
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+lv_obj_t * myButton;
+lv_obj_t * myButtonLabel;
+lv_obj_t * myLabel;
+
+lv_style_t myButtonStyleREL; //relesed style
+lv_style_t myButtonStylePR; //pressed style
+
+static lv_res_t btn_click_action(lv_obj_t * btn)
+{
+	uint8_t id = lv_obj_get_free_num(btn); //id usefull when there are multiple buttons
+
+    if(id == 0)
+    {
+    	char buffer[100];
+ 		sprintf(buffer, "button was clicked %i milliseconds from start", pros::millis());
+ 		lv_label_set_text(myLabel, buffer);
+    }
+
+    return LV_RES_OK;
+}
+
+void initialize()
+{
+	lv_style_copy(&myButtonStyleREL, &lv_style_plain);
+    myButtonStyleREL.body.main_color = LV_COLOR_MAKE(200, 200, 255);
+	myButtonStyleREL.body.grad_color = LV_COLOR_MAKE(200, 200, 255);
+    myButtonStyleREL.body.radius = 0;
+    myButtonStyleREL.text.color = LV_COLOR_MAKE(255, 255, 255);
+
+    lv_style_copy(&myButtonStylePR, &lv_style_plain);
+    myButtonStylePR.body.main_color = LV_COLOR_MAKE(255, 255, 255);
+	myButtonStylePR.body.grad_color = LV_COLOR_MAKE(255, 255, 255);
+    myButtonStylePR.body.radius = 0;
+    myButtonStylePR.text.color = LV_COLOR_MAKE(255, 255, 255);
+
+    myButton = lv_btn_create(lv_scr_act(), NULL); //create button, lv_scr_act() is deafult screen object
+    lv_obj_set_free_num(myButton, 0); //set button is to 0
+    lv_btn_set_action(myButton, LV_BTN_ACTION_CLICK, btn_click_action); //set function to be called on button click
+    lv_btn_set_style(myButton, LV_BTN_STYLE_REL, &myButtonStyleREL); //set the relesed style
+    lv_btn_set_style(myButton, LV_BTN_STYLE_PR, &myButtonStylePR); //set the pressed style
+    lv_obj_set_size(myButton, 20000, 50); //set the button size
+    lv_obj_align(myButton, NULL, LV_ALIGN_IN_RIGHT_MID, -20, 0); //set the position to top mid
+
+    myButtonLabel = lv_label_create(myButton, NULL); //create label and puts it inside of the button
+    lv_label_set_text(myButtonLabel, "Click the Button"); //sets label text
+
+    myLabel = lv_label_create(lv_scr_act(), NULL); //create label and puts it on the screen
+    lv_label_set_text(myLabel, "Button has not been clicked yet"); //sets label text
+    lv_obj_align(myLabel, NULL, LV_ALIGN_IN_LEFT_MID, 10, 0); //set the position to center
 }
 
 /**
@@ -54,21 +94,19 @@ void autonomous() {}
  * If the robot is disabled or communications is lost, the
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
- */
+*/
+#define LEFT_WHEELS_PORT 1
+#define RIGHT_WHEELS_PORT 10
+
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+ 	pros::Motor left_wheels (LEFT_WHEELS_PORT);
+   	pros::Motor right_wheels (RIGHT_WHEELS_PORT, true);
+   	pros::Controller controller (CONTROLLER_MASTER);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+   	while (true) {
+     	left_wheels.move(controller.get_analog(ANALOG_LEFT_Y));
+     	right_wheels.move(controller.get_analog(ANALOG_RIGHT_Y));
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
-	}
+     	pros::delay(2);
+   	}
 }
