a4686152065705a7f26bfb3c1f4cda2b5dec2712
Drusketh <zenithdruskin@gmail.com>
2019-10-31 16:15:21 -0700

    Display and more Movement code

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,27 @@
 #include "main.h"
+#include "display/lvgl.h"
+
+#include "logo.c"
+
 
 lv_obj_t * autonBtn;
 lv_obj_t * autonBtnLabel;
+lv_obj_t * testBtn;
+lv_obj_t * testBtnLabel;
 lv_obj_t * myLabel;
 
-lv_style_t autonBtnStyleREL; //relesed style
-lv_style_t autonBtnStylePR; //pressed style
+lv_style_t autonBtnStyleREL;
+lv_style_t autonBtnStylePR;
 
 static lv_res_t btn_click_action(lv_obj_t * btn)
 {
-	uint8_t id = lv_obj_get_free_num(btn); //id usefull when there are multiple buttons
+	uint8_t id = lv_obj_get_free_num(btn);
 
     if(id == 0)
     {
     	char buffer[100];
  		sprintf(buffer, "button was clicked %i milliseconds from start", pros::millis());
- 		lv_label_set_text(myLabel, buffer);
+ 		//lv_label_set_text(myLabel, buffer);
     }
 
     return LV_RES_OK;
@@ -24,8 +30,8 @@ static lv_res_t btn_click_action(lv_obj_t * btn)
 void initialize()
 {
 	lv_style_copy(&autonBtnStyleREL, &lv_style_plain);
-    autonBtnStyleREL.body.main_color = LV_COLOR_MAKE(200, 200, 255);
-	autonBtnStyleREL.body.grad_color = LV_COLOR_MAKE(200, 200, 255);
+    autonBtnStyleREL.body.main_color = LV_COLOR_MAKE(0, 255, 0);
+	autonBtnStyleREL.body.grad_color = LV_COLOR_MAKE(0, 255, 0);
     autonBtnStyleREL.body.radius = 0;
     autonBtnStyleREL.text.color = LV_COLOR_MAKE(255, 255, 255);
 
@@ -35,20 +41,31 @@ void initialize()
     autonBtnStylePR.body.radius = 0;
     autonBtnStylePR.text.color = LV_COLOR_MAKE(255, 255, 255);
 
-    autonBtn = lv_btn_create(lv_scr_act(), NULL); //create button, lv_scr_act() is deafult screen object
-    lv_obj_set_free_num(autonBtn, 0); //set button is to 0
-    lv_btn_set_action(autonBtn, LV_BTN_ACTION_CLICK, btn_click_action); //set function to be called on button click
-    lv_btn_set_style(autonBtn, LV_BTN_STYLE_REL, &autonBtnStyleREL); //set the relesed style
-    lv_btn_set_style(autonBtn, LV_BTN_STYLE_PR, &autonBtnStylePR); //set the pressed style
-    lv_obj_set_size(autonBtn, 200, 50); //set the button size
-    lv_obj_align(autonBtn, NULL, LV_ALIGN_IN_TOP_RIGHT, -10, 10); //set the position to top mid
+    autonBtn = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_free_num(autonBtn, 0);
+    lv_btn_set_action(autonBtn, LV_BTN_ACTION_CLICK, btn_click_action);
+    lv_btn_set_style(autonBtn, LV_BTN_STYLE_REL, &autonBtnStyleREL);
+    lv_btn_set_style(autonBtn, LV_BTN_STYLE_PR, &autonBtnStylePR);
+    lv_obj_set_size(autonBtn, 200, 105);
+    lv_obj_align(autonBtn, NULL, LV_ALIGN_IN_TOP_RIGHT, -10, 10);
 
-    autonBtnLabel = lv_label_create(autonBtn, NULL); //create label and puts it inside of the button
-    lv_label_set_text(autonBtnLabel, "Autonomous"); //sets label text
+    autonBtnLabel = lv_label_create(autonBtn, NULL);
+    lv_label_set_text(autonBtnLabel, "Autonomous");
 
-    myLabel = lv_label_create(lv_scr_act(), NULL); //create label and puts it on the screen
-    lv_label_set_text(myLabel, "Button has not been clicked yet"); //sets label text
-    lv_obj_align(myLabel, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0); //set the position to center
+	testBtn = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_free_num(testBtn, 0);
+    lv_btn_set_action(testBtn, LV_BTN_ACTION_CLICK, btn_click_action);
+    lv_btn_set_style(testBtn, LV_BTN_STYLE_REL, &autonBtnStyleREL);
+	lv_btn_set_style(testBtn, LV_BTN_STYLE_PR, &autonBtnStylePR);
+    lv_obj_set_size(testBtn, 200, 105);
+    lv_obj_align(testBtn, NULL, LV_ALIGN_IN_BOTTOM_RIGHT, -10, -10);
+
+    testBtnLabel = lv_label_create(testBtn, NULL);
+    lv_label_set_text(testBtnLabel, "Settings");
+
+    myLabel = lv_label_create(lv_scr_act(), NULL);
+    //lv_label_set_text(myLabel, "Button has not been clicked yet");
+    lv_obj_align(myLabel, NULL, LV_ALIGN_IN_LEFT_MID, 0, 0);
 }
 
 /**
@@ -97,16 +114,48 @@ void autonomous() {}
 */
 #define LEFT_WHEELS_PORT 1
 #define RIGHT_WHEELS_PORT 10
+#define L_ARM_PORT 4
+#define R_ARM_PORT 7
+#define L_INTAKE_PORT 12
+#define R_INTAKE_PORT 14
 
 void opcontrol() {
  	pros::Motor left_wheels (LEFT_WHEELS_PORT);
    	pros::Motor right_wheels (RIGHT_WHEELS_PORT, true);
    	pros::Controller controller (CONTROLLER_MASTER);
+		pros::Motor l_arm (R_ARM_PORT, MOTOR_GEARSET_36);
+		pros::Motor r_arm (L_ARM_PORT, MOTOR_GEARSET_36);
+		pros::Motor l_intake (L_INTAKE_PORT, MOTOR_GEARSET_36);
+		pros::Motor r_intake (R_INTAKE_PORT, MOTOR_GEARSET_36);
 
-   	while (true) {
+	while (true) {
      	left_wheels.move(controller.get_analog(ANALOG_LEFT_Y));
      	right_wheels.move(controller.get_analog(ANALOG_RIGHT_Y));
 
+		if (controller.get_digital(DIGITAL_R1)) {
+			l_arm.move_velocity(100);
+			r_arm.move_velocity(100);
+		}
+
+		else if (controller.get_digital(DIGITAL_R2)) {
+			l_arm.move_velocity(-100);
+			r_arm.move_velocity(-100);
+		}
+
+		else {
+			l_arm.move_velocity(0);
+			r_arm.move_velocity(0);
+		}
+
+		if (controller.get_digital(DIGITAL_L1)) {
+			l_intake.move_velocity(100);
+			r_intake.move_velocity(100);
+		}
+
+		else if (controller.get_digital(DIGITAL_L2)) {
+			l_intake.move_velocity(-100);
+			r_intake.move_velocity(-100);
+		}
      	pros::delay(2);
    	}
 }
